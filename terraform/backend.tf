cb435b33b1f3a3ce17034e1da67519ff208912b4
Lucian Dragos <104193058+luciandragos@users.noreply.github.com>
2025-04-26 18:41:20 +0200

    fix: remove duplicate provider

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,10 +2,4 @@ terraform {
   backend "gcs" {
     bucket = "fs-ai-gul-kai-tfstate-firestarter-x"
   }
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "6.31.0"
-    }
-  }
 }
